42e1c6d789e423b65b7e8ea9500c4d31099a1f6b
Evrajody <evrajody>
2025-08-22 10:08:03 +0100

    roll vue back

---
+++ pages/indexo.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+const {$sisepApi} = useNuxtApp()
+
+</script>
+
+<template>
+
+  <header class="min-h-[calc(100vh-4rem)] flex flex-col justify-center relative bg-green-400"
+          style='background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.6)), url("https://www.gouv.bj/upload/images/articles/ckeditor/amazone2.jpg"); background-size: cover; background-position: center center;'>
+
+    <!--    <div class="inset-0 absolute z-5 bg-black/20"></div>-->
+
+    <!--    <img alt="" class="absolute inset-0 w-full h-full"-->
+    <!--        src="https://www.gouv.bj/upload/images/articles/ckeditor/amazone2.jpg">-->
+
+    <div class="max-w-5xl px-5 w-full mx-auto inset-0">
+
+
+      <div class="flex flex-col space-y-5">
+
+        <h2 class="text-6xl max-w-7xl text-white text-center leading-tight py-5 font-bold">
+          Accéder aux données clés sur le cadre de vie au Bénin
+        </h2>
+
+        <p class="text-lg font-medium">
+          Retrouver les faits et informations sur les secteurs d'activité liés au cadre de vie et au développement
+          durable.
+        </p>
+
+      </div>
+
+
+    </div>
+
+  </header>
+
+
+</template>
+
+<style scoped>
+
+</style>
