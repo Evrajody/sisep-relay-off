597d3121f866772d4140ae5680d88828cf266bb5
evrajodygildas <evrajodygildas@gmail.com>
2025-10-17 13:08:05 +0100

    maj test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -60,7 +60,7 @@ export default defineNuxtConfig({
         globalAppMiddleware: false,
         isEnabled: true,
         originEnvKey: "AUTH_ORIGIN",
-        baseURL: `${process.env.NUXT_AUTH_ORIGIN}/api/auth`,
+        baseURL: `${process.env.NUXT_AUTH_ORIGIN}/local/auth`,
         sessionRefresh: {
             enablePeriodically: true,
             enableOnWindowFocus: true,
